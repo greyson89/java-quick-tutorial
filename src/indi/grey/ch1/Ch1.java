591b69d0f4a738897ac6a4011c43b1be0540ae40
Greyson-MBP <greyson.chen@icloud.com>
2022-10-04 16:48:58 +0800

    first commit

---
+++ src/indi/grey/ch1/Ch1.java
@@ -0,0 +1,21 @@
+package indi.grey.ch1;
+
+public class Ch1 {
+	
+
+	public static void main(String[] args) {
+		
+
+		short s = 1;
+		int i = 1;
+		long l = 1;
+		i = 2147483647;
+
+		i++;
+
+		System.out.println("i = "+i);
+
+
+	}
+	
+}
